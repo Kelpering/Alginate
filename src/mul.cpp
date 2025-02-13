9a7f52b2f506ef432961ac8b81bfde4bf941cf1b
Kelpering <tristan8272@gmail.com>
2025-02-13 09:00:05 -0700

    mul_short

--- src/mul.cpp
+++ src/mul.cpp
@@ -15,17 +15,33 @@ void AlgInt::mul(const AlgInt& x, const AlgInt& y, AlgInt& ret, bool unsign)
 void AlgInt::mul(const AlgInt& x, uint32_t y, AlgInt& ret, bool unsign)
 {
     // Basic sign handle (sign^sign)
+    AlgInt temp;
+    temp.resize(x.size+1);
+    temp.sign = x.sign && !unsign;
 
     // Loop block (single digit w/ carry)
+    uint32_t carry = 0;
+    for (size_t i = 0; i < temp.size; i++)
+    {
+        uint64_t calc = (uint64_t) x.num[i] * y + carry;
+        
+        carry = (calc >> 32);
+        ret.num[i] +=  (uint32_t) calc;
+    }
+
+    // Remove leading zeroes.
+    temp.trunc();
+
+    // Set ret = temp
+    AlgInt::swap(ret, temp);
 
     return;
 }
 
 void AlgInt::mul(uint32_t, const AlgInt& y, AlgInt& ret, bool unsign)
 {
-    // Basic sign handle (sign^sign)
-
-    // Loop block (single digit w/ carry)
+    // x * y == y * x
+    mul(y, x, ret, unsign);
 
     return;
 }
