dd6dce15acb59a7ca03493cd59bd70ecd13096d5
Kelpering <tristan8272@gmail.com>
2025-02-27 08:07:54 -0700

    Created input.cpp

---
+++ src/input.cpp
@@ -0,0 +1,17 @@
+/**
+*   File: input.cpp
+*   Project: Alginate
+*   SPDX-License-Identifier: Unlicense
+* 
+*   input.cpp contains simple methods to provide convenint user 
+*   input for AlgInts. This includes initializer methods for
+*   inputs too complex to be used in a constructor.
+*/
+#include "Alginate.hpp"
+
+// PKCS#1 input
+    // Make PKCS#1 output (and other array outputs)
+
+// string input (we have it mostly written)
+
+// 
